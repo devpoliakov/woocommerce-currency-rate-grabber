8abf6926783329b6293c9c692751fe4ab46b5377
Yurii Poliakov <kontynent@ua.fm>
2021-03-11 19:04:36 +0200

    Show rate at the option page

--- inc/options_page.php
+++ inc/options_page.php
@@ -4,6 +4,17 @@ function currency_rate_options_page(){
 	?>
 <div class="wrap testusBlank-for-orders">
 <h1><?php echo __("Grabber configuration") ?></h1>
+<?php
+
+$currency_source = json_decode(get_option('wc_exchange_rate'));
+
+echo "<p><strong>your currency</strong>: " . $currency_source->name. " </p>";
+echo "<p><strong>your USD/QAR rate</strong>: " . round($currency_source->inverseRate, 2) ."</p>";
+echo "<p><strong>your QAR/USD rate</strong>: " . round(1 / $currency_source->inverseRate, 2) ."</p>";
+echo "<p><strong>last request</strong>: " . $currency_source->date. " </p>";
+
+
+?>
 
 </div>
 	<?php
